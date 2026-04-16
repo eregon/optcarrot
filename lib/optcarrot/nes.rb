40b1e786742e18393715057510c2809f09f11b95
Benoit Daloze <eregontp@gmail.com>
2026-04-16 15:28:30 +0200

    Revert "Show max FPS"
    * This reverts commit d16f78f2c2df033b9deccd52e8b0f0d5c8b569a9.
    * Not representative of the rest of the run.

--- lib/optcarrot/nes.rb
+++ lib/optcarrot/nes.rb
@@ -19,8 +19,6 @@ module Optcarrot
 
       @frame = 0
       @frame_target = @conf.frames == 0 ? nil : @conf.frames
-      @fps = 0.0
-      @max_fps = 0.0
       @fps_history = [] if save_fps_history?
     end
 
@@ -50,7 +48,6 @@ module Optcarrot
 
       @input.tick(@frame, @pads)
       @fps = @video.tick(@ppu.output_pixels, @frame)
-      @max_fps = @fps if @fps > @max_fps
       @fps_history << @fps if save_fps_history?
       @audio.tick(@apu.output)
 
@@ -70,7 +67,6 @@ module Optcarrot
         end
         puts RUBY_DESCRIPTION
         puts "fps: #{ @fps }" if @conf.print_fps
-        puts "max fps: #{@max_fps}"
       end
       if @conf.print_video_checksum && @video.instance_of?(Video)
         puts "checksum: #{ @ppu.output_pixels.pack("C*").sum }"
